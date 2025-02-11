e050599ac635ec570c06002c822cee092d665abe
junayed-hassan <junayedhassan102045@gmail.com>
2025-02-11 15:43:42 +0600

    header dan

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./component/Header";
+import Home from "./Pages/Home";
 
 function App() {
 
   return (
     <>
-    <Header/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={"/"} exact={true} element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header>
-            <div className='top-strip py-2 border-t-1 border-b-1 border-[#0001]'>
+            <div className='top-strip py-2 border-y-1 border-gray-250'>
                 <div className='container'>
                     <div className='flex items-center justify-between'>
                         <div className='col-1 w-[50%]'>
-                            <p className='text-sm font-medium'>Get up to 50% off new season styles. limited time only</p>
+                            <p className='text-xs font-medium'>Get up to 50% off new season styles. limited time only</p>
                         </div>
 
                         <div className='col-2 flex items-center justify-end'>
-                            <ul>
-                                <li>
-                                    <Link to="login"></Link>
+                            <ul className="flex items-center gap-3">
+                                <li className="">
+                                    <Link to="/help-center" className="text-sm font-medium hover:text-rose-400 transition">Help Center</Link>
+                                </li>
+                                <li className="">
+                                    <Link to="/order-tracking" className="text-sm font-medium hover:text-rose-400 transition">Order Tracking</Link>
                                 </li>
                             </ul>
                         </div>
