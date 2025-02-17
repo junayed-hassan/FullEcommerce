5782f65a7e7f6921ce7ab8d46f299502ff4d4deb
junayed-hassan <junayedhassan102045@gmail.com>
2025-02-17 21:19:28 +0600

    up

--- src/component/Header/Navigation/index.jsx
+++ src/component/Header/Navigation/index.jsx
@@ -1,8 +1,19 @@
+import { Button } from "@mui/material";
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
 function Navigation() {
-  return (
-    <div>Navigation</div>
-  )
+    return (
+        <nav>
+            <div className="container flex items-center justify-end">
+                <div className="col_1 w-[30%]">
+                    <Button className="!text-black gap-2"><MenuOpenIcon/> Shop By Categories</Button>
+                </div>
+                <div className="col_2 w-[70%]">
+                
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navigation;
